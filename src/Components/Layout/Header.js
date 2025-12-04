718f612c4dca6516ff3be145762baa4b6122ebbf
Abhishek Soni <soni.abhishek@velocis.in>
2025-12-04 19:49:50 +0530

    manage code

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import mealsImage from '../../assets/meal2.jpg';
+import classes from '../../Components/Layout/Header.module.css';
+const Header = props =>{
+    return <>
+    <Fragment>
+        <header className={classes.header}>
+            <h1>React Meals</h1>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt='a table full of delicious meal'/>
+        </div>
+    </Fragment>
+    </>
+}
+
+export default Header;
